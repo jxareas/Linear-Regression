636afad132eedee3ab090a2623b0de1124ded186
jxareas <areasjx@gmail.com>
2021-07-19 01:57:23 -0600

    Added Regression through the Origin model

---
+++ scripts/Module1/01_GaltonData.R
@@ -0,0 +1,57 @@
+# Packages ----------------------------------------------------------------
+
+require(ggplot2)
+require(UsingR)
+require(tidyr)
+require(dplyr)
+require(ggthemes)
+
+# Data --------------------------------------------------------------------
+
+data(galton)
+
+galton <- galton |>
+        pivot_longer(everything(),
+                     names_to = "status",
+                     values_to = "height") |>
+        mutate(status = str_to_title(status))
+
+# Boxplot --------------------------------------------------------
+
+ggplot(data = galton,
+       mapping = aes(x = status, y = height, fill = status)) +
+        stat_boxplot(geom = "errorbar", size = 0.8) +
+        geom_boxplot(size = 0.8) +
+        labs(title = "Height of Children & Parents",
+             x = "Status",
+             y = "Height") +
+        scale_fill_viridis_d(option = "magma", begin = 0.8) +
+        theme_solarized() +
+        theme(
+                plot.title = element_text(face = "bold",
+                                          color = "black",
+                                          hjust = 0.5),
+                axis.title.x = element_text(face = "bold"),
+                axis.title.y = element_text(face = "bold")
+        ) +
+        guides(fill = "none")
+
+# Histogram --------------------------------------------------------------------
+
+ggplot(data = galton, mapping = aes(x = height, fill = status)) +
+        geom_histogram(binwidth = 1, color = "black") +
+        facet_wrap(. ~ status) +
+        scale_fill_viridis_d(option = "cividis", alpha = 0.6) +
+        labs(title = "Height Histogram",
+             x = "Height (Inches)",
+             y = "Frequency",
+             fill = "Status") +
+        theme_stata() +
+        theme(
+                plot.title = element_text(face = "bold", hjust = 0.5),
+                axis.title.x = element_text(face = "bold"),
+                axis.title.y = element_text(face = "bold")
+        ) +
+        guides(fill = "none")
+
+
---
+++ scripts/Module1/02_LinearRegressionOrigin.R
@@ -0,0 +1,132 @@
+# Packages ----------------------------------------------------------------
+
+require(ggplot2)
+require(ggthemes)
+require(UsingR)
+require(dplyr)
+
+# Data --------------------------------------------------------------------
+
+data(galton)
+
+frequency <- as.data.frame(table(galton$child, galton$parent)) |>
+        mutate(across(everything(), .fns = ~ as.numeric(.x)))
+
+names(frequency) <- c("child", "parent", "freq")
+
+
+# Scatterplot -------------------------------------------------------------
+
+ggplot(frequency |>
+               filter(freq > 0),
+       mapping = aes(
+               x = parent,
+               y = child,
+               size = freq,
+               col = freq
+       )) +
+        geom_point() +
+        geom_smooth(
+                color = "yellow",
+                size = 2,
+                method = "lm",
+                se = T
+        ) +
+        scale_color_viridis_c(begin = 0.2,
+                              end = 0.8,
+                              option = "plasma") +
+        labs(title = "Bubbleplot: Height of Parents vs Children",
+             x = "Parents (Inches)",
+             y = "Children (Inches)") +
+        theme(
+                plot.title = element_text(
+                        face = "bold",
+                        hjust = 0.5,
+                        color = "purple4"
+                ),
+                axis.title.x = element_text(face = "bold.italic"),
+                axis.title.y = element_text(face = "bold.italic"),
+                legend.position = "none"
+        )
+
+
+# Mean Centering Height ---------------------------------------------------
+
+galton <- galton |>
+        mutate(#Mean Centering Variables
+                mc_child = child - mean(child),
+                mc_parent = parent - mean(parent))
+mc_frequency <-
+        as.data.frame(table(galton$mc_child, galton$mc_parent)) |>
+        apply(2, as.numeric) |>
+        as_tibble()
+
+names(mc_frequency) <- c("child", "parent", "freq")
+
+
+# Scatterplot: Mean Centered Variables ------------------------------------
+
+ggplot(mc_frequency |>
+               filter(freq > 0),
+       mapping = aes(
+               x = parent,
+               y = child,
+               size = freq,
+               col = freq
+       )) +
+        geom_point() +
+        geom_smooth(
+                color = "yellow",
+                size = 2,
+                method = "lm",
+                se = T
+        ) +
+        scale_color_viridis_c(begin = 0.2,
+                              end = 0.8,
+                              option = "plasma") +
+        labs(title = "Bubbleplot: Height of Parents vs Children",
+             x = "Parents (Inches)",
+             y = "Children (Inches)") +
+        theme(
+                plot.title = element_text(
+                        face = "bold",
+                        hjust = 0.5,
+                        color = "purple4"
+                ),
+                axis.title.x = element_text(face = "bold.italic"),
+                axis.title.y = element_text(face = "bold.italic"),
+                legend.position = "none"
+        )
+
+
+# Simple Linear Regression Model ------------------------------------------
+
+data(galton)
+galton <- galton |> 
+        mutate(
+                mc_child = child - mean(child),
+                mc_parent = parent - mean(parent)
+        )
+
+# Simple Linear Regression
+# B0 ~ 23.94 (Not equal to zero)
+# B1 ~ 0.6463
+model <- lm(
+        data = galton,
+        formula = child ~ parent
+)
+
+
+# Forcing Regression through the Origin
+# B0 = 0. (Assumed by the model)
+# B1 ~ 0.6463
+origin_model <-
+        lm(data = galton,
+           formula = I(child - mean(child)) ~ I(parent - mean(parent)) - 1)
+
+# Regression using Mean Centered Variables
+# B0 is approximately 0.
+# B1 ~ 0.6463
+mc_model <- 
+        lm(data = galton,
+           formula = mc_child ~ mc_parent)
